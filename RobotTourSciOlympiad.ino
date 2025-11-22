f2579c8ce33ddfadc16995a89b1f6111119fb3ff
sirbobisherenow <chetan.likes.nature@gmail.com>
2025-11-22 15:50:01 -0800

    more straight forward driving

--- RobotTourSciOlympiad.ino
+++ RobotTourSciOlympiad.ino
@@ -1,53 +1,53 @@
-
-
-//#include <MeOrion.h>
-#include <MeMCore.h>
-#include <Arduino.h>
-#include <Wire.h>
-#include <SoftwareSerial.h>
-
-int turnfactor = 850;
-// The two motor objects
-MeDCMotor motor1(M1);
-MeDCMotor motor2(M2);
-
-void setup()
-{
-  
-}
-void forward(float seconds){
-  motor1.run(-205);
-  motor2.run(200);
-  delay(seconds);
-  motor1.stop();
-  motor2.stop();
-  delay(100);
-}
-void turnLeft(){
-  motor1.run(100);
-  motor2.run(100);
-  delay(turnfactor);
-  motor1.stop();
-  motor2.stop();
-  delay(200);
-}
-void turnRight(){
-  motor1.run(-100);
-  motor2.run(-100);
-  delay(turnfactor);
-  motor1.stop();
-  motor2.stop();
-  delay(200);
-}
-void Main(){
-  forward(20000);
-}
-void loop(){
-  pinMode(A7, INPUT);
-  while(1) {
-      if((0 ^ (analogRead(A7) > 10 ? 0 : 1))){
-        delay(1000);
-        Main();
-      }
-  }
+
+
+//#include <MeOrion.h>
+#include <MeMCore.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+int turnfactor = 850;
+// The two motor objects
+MeDCMotor motor1(M1);
+MeDCMotor motor2(M2);
+
+void setup()
+{
+  
+}
+void forward(float seconds){
+  motor1.run(-215);
+  motor2.run(200);
+  delay(seconds);
+  motor1.stop();
+  motor2.stop();
+  delay(100);
+}
+void turnLeft(){
+  motor1.run(100);
+  motor2.run(100);
+  delay(turnfactor);
+  motor1.stop();
+  motor2.stop();
+  delay(200);
+}
+void turnRight(){
+  motor1.run(-100);
+  motor2.run(-100);
+  delay(turnfactor);
+  motor1.stop();
+  motor2.stop();
+  delay(200);
+}
+void Main(){
+  forward(20000);
+}
+void loop(){
+  pinMode(A7, INPUT);
+  while(1) {
+      if((0 ^ (analogRead(A7) > 10 ? 0 : 1))){
+        delay(1000);
+        Main();
+      }
+  }
 }
